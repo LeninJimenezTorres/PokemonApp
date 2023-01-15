40eb3d3376e472def51dade8bd2af2ab6d57b4fd
Lenin Jimenez Torres <jlenin2527@gmail.com>
2023-01-15 00:27:24 -0500

    Preview and Info Markup + API connection, Next step, final GUI and Search functionality

--- src/Components/Allcards.js
+++ src/Components/Allcards.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import CardPokemon from './CardPokemon'
 import '../Styles/Allcards.css';
 
@@ -6,21 +6,63 @@ import PokemonInfo from './PokemonInfo';
 
 const Allcards = ({results}) => {
     //console.log(results)
-    const [urlPreview, setUrlPreview] = useState(null);
+    const [urlPreview, setUrlPreview] = useState(null)
+    const [preview, setpreview] = useState(false)
+    const [anterior, setanterior] = useState(null)
+    const [nuevo, setnuevo] = useState(null)
+    const [cerrar, setcerrar] = useState(false)
+    useEffect(() => {
+        if(preview==true){
+            document.documentElement.style.setProperty('--preview-visibility', 'visible');   
+            document.documentElement.style.setProperty('--width-preview', '80vw');   
+            document.documentElement.style.setProperty('--width-cards', '20vw');   
+            document.documentElement.style.setProperty('--columns', '2');
+            setcerrar(false)
+        }
+        if((!preview && (anterior==nuevo)) || (cerrar) ){
+            document.documentElement.style.setProperty('--preview-visibility', 'hidden');   
+            document.documentElement.style.setProperty('--width-preview', '0vw');   
+            document.documentElement.style.setProperty('--width-cards', '100vw');   
+            document.documentElement.style.setProperty('--columns', '4');
+        }
+    })
+    
     return (
         <div className='allcards-container'>
             <ul>
                 {
                     results.map(p=>
-                        <li key={p.name} onClick={()=>setUrlPreview(p.url) }>
+                        <li key={p.name} onClick={()=>{
+                            setnuevo(p.url)
+                            setUrlPreview(p.url)
+                            setanterior(nuevo)
+                            if(preview){
+                                setpreview(false)
+                            }   
+                            else {
+                                setpreview(true)
+                            }
+
+                            console.log('Estados: [Preview:'+preview+'], [Cerrar:'+cerrar+']');
+                        } }>
                             <CardPokemon url={p.url}/>
                         </li>
                     )
                 }
             </ul>
+            <div className='close' onClick={()=>{
+                console.log('CLOSEEE')
+                if(preview==true){
+                    setpreview(false)
+                    setcerrar(true)
+                }
+                console.log('Estados: [Preview:'+preview+'], [Cerrar:'+cerrar+']');
+                        
+            }}></div>
             <div className='preview'>
                 <PokemonInfo url={urlPreview}/>
             </div>
+            <div className='preview-space'></div>
         </div>
     )
 }
--- src/Components/PokemonInfo.js
+++ src/Components/PokemonInfo.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../Styles/PokemonInfo.css';
 import UseFetch from '../Functions/UseFetch';
 function PokemonInfo({url}) {
     const estado = UseFetch(url)
     const {cargando,data}=estado
     console.log(data)
+
+    //const [sprites, setsprites] = useState([])
+    let sprites=[]
+    if(data!=null){
+        let spritesObject = Object.values(data.sprites);
+        //console.log(sprites)
+        
+        for(let i=0; i< spritesObject.length; i++){
+            //console.log(typeof sprites[i]);
+            if(spritesObject[i]!=null && typeof spritesObject[i]=='string'){
+                //setsprites([...sprites,spritesObject[i]])
+                sprites=[...sprites,spritesObject[i]]
+            }
+        }
+        console.log('Todos los sprites')
+        console.log(sprites);
+    }
     
     
     return (
@@ -15,12 +32,52 @@ function PokemonInfo({url}) {
                     :
                     <div className='info-in'>
                         <img src={data.sprites.front_default} alt=""/>
-                        <p className='numero-info'>{data.id}</p>
-                        <p className='nombre-info'>{data.name}</p>
-                        <p className='types'>{data.name}</p>
-                        <p className='peso'>{data.weight}</p>
-                        <p className='sprites'>Sprites</p>
-                        <p className='movimientos'>{data.weight}</p>
+                        <div className='numero-info'>
+                            <p>#</p>
+                            <p>{data.id}</p>
+                        </div>
+                        <div className='nombre-info'>
+                            <p>{data.name}</p>
+                        </div>
+                        <div className='types'>
+                            <p>Types</p>
+                            <>
+                                {
+                                    data.types.map(item=>
+                                        <p>{item.type.name} </p>
+                                    )
+                                }
+                            </>
+                        </div>
+                        <div className='peso'>
+                            <p>Peso</p>
+                            <p><br/>{data.weight}</p>
+                        </div>
+                        <div className='sprites'>
+                            <p>Sprites</p>
+                            <ul>
+                                {
+                                    sprites.length>0?
+                                    sprites.map(item=>
+                                        <div className='sprite-in'>
+                                            <img src={item} alt=""/>
+                                        </div>
+                                    )
+                                    :
+                                    console.log('Cargando')
+                                }
+                            </ul>
+                        </div>
+                        <div className='movimientos'>
+                            <p>Movimientos</p>
+                            <>
+                                {
+                                    data.moves.map(item=>
+                                        <p>{item.move.name} </p>
+                                    )
+                                }
+                            </>
+                        </div>
                     </div>
                 }
         </div>
